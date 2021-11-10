cf946d17bfd4755a0e3a76d396823808a1ecd793
TOneal419 <tyler.michael.oneal@gmail.com>
2021-11-10 16:05:28 -0600

    Adding extra files for support

--- SecCure/api/serializers.py
+++ SecCure/api/serializers.py
@@ -7,11 +7,13 @@ class PwnSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Pwn
-        fields = ('Name', 'Domain', 'BreachDate', 'Description')
+        
 
         Name = serializers.CharField(max_length=200)
         Domain = serializers.CharField(max_length=200)
         BreachDate = serializers.DateField()
         Description = serializers.CharField(max_length=500)
+
+        fields = ('Name', 'Domain', 'BreachDate', 'Description')
 
        #fields = ('id', 'email','created_at') #add more fields as they are created
--- SecCure/api/views.py
+++ SecCure/api/views.py
@@ -29,7 +29,7 @@ class PwnView(generics.CreateAPIView):
                 data = r.json()
                 #serializer_var = PwnSerializer(r.json())                
                 return Response(r.json(), status=status.HTTP_200_OK)               # THIS IS WORKING! 
-                #return Response(serializer_var.data, status=status.HTTP_200_OK)                                      
+                #return Response(serializer_var.data, status=status.HTTP_200_OK)    # returning nothing (empty json)                                  
             else:
                 attempt_num += 1
                 # You can probably use a logger to log the error here
