2022ad10af7348135780bc13a60f7dc6f3b9fd3a
TOneal419 <tyler.michael.oneal@gmail.com>
2021-11-16 09:48:16 -0600

    Removed Debug

--- SecCure/SecCure/settings.py
+++ SecCure/SecCure/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$+9l*m8z=n_0wyi6rj_n5ln(sfm!gbdxjqls^&g=m%mjb&94!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'SecCure.urls'
