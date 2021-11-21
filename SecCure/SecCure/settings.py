61de1dc33cb615f288f004470ba967df41ee27d9
TOneal419 <56747323+TOneal419@users.noreply.github.com>
2021-11-20 18:14:47 -0600

    Update settings.py

--- SecCure/SecCure/settings.py
+++ SecCure/SecCure/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$+9l*m8z=n_0wyi6rj_n5ln(sfm!gbdxjqls^&g=m%mjb&94!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
