eb2a6314c2b3244e0db14516654a4ed439d2d034
Cutinha <arc4799@ts.tamu.edu>
2021-12-05 17:54:06 -0600

    Scroll function js

--- SecCure/frontend/static/frontend/main.js
+++ SecCure/frontend/static/frontend/main.js
@@ -1,4 +1,17 @@
 /*! For license information please see main.js.LICENSE.txt */
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 80) {        
+        $('#return-to-top').fadeIn(200);    
+    } else {
+        $('#return-to-top').fadeOut(200);   
+    }
+});
+$('#return-to-top').click(function() {      
+    $('body,html').animate({
+        scrollTop : 0                       
+    }, 500);
+});
+
 function playAudio(value) {
     var audio = document.getElementById(String(value));
     audio.play();
